1d490b3440654a34fab1995adfc4f51dcf24a274
vedat73 <vedat.ozlu@outlook.com>
2023-07-15 19:58:58 +0300

    Initial Commit

---
+++ NewStart/NewStartApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewStartApp.swift
+//  NewStart
+//
+//  Created by Vedat Ozlu on 15.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewStartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
